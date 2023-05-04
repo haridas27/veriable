b0d210b15263ba63a7f624105c19b3470bc866d8
haridas27 <haridasglc45@gmail.com>
2023-05-04 23:26:00 +0530

    veriable

---
+++ app.js
@@ -0,0 +1,38 @@
+// var mark = 95
+// var mark = 34+45
+// console.log(mark);
+
+// let stady = 98+60
+//     stady = 50+60 
+// console.log(stady);
+ 
+
+// const std = 100+10
+// const std = 19
+// console.log(std);
+
+
+
+// const std = 5 ; {
+//     a : 8 + [
+//         banana = 9,
+//         apple = 4,
+//         oreng = 6,
+//     ]
+// }
+// console.log(std + apple + banana + oreng);
+
+
+// const abc = 7 ; {
+//     d : 5 + [
+//         bavana = 6,
+//         apple = 9,
+//         oreng = 5,
+//     ]
+// }
+// console.log(abc + apple );
+
+
+
+const a = 9 
+console.log(a);
